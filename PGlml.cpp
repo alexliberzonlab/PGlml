02700424b9ec12479175564967a501e99984cf40
Jeremie Knuesel <knuesel@gmail.com>
2017-10-13 18:42:27 +0200

    set pglml_debug = 1

--- PGlml.cpp
+++ PGlml.cpp
@@ -92,8 +92,8 @@ a> for the whole program
 
 //debug display (0- quiet, 1- only final image (and only if -O true), 2- image step by step)
 //#define pglml_debug 0
-//#define pglml_debug 1
-#define pglml_debug 2
+#define pglml_debug 1
+//#define pglml_debug 2
 #include "../CImg/CImg.h"
 
 using namespace cimg_library;
